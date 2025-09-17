1aa2d3f0d1a714b92d7e744e6e3d944ea3447788
Deivison Santos <coder08@gsomosocta.com>
2025-09-17 15:27:19 -0300

    Fix: dash

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Dashboard - Prefeitura')
+@section('title', 'Dashboard - Licicon')
 
 @section('content')
 <div class="py-6">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <!-- Cabeçalho -->
         <div class="mb-8">
-            <h2 class="text-2xl font-bold text-gray-800">Painel da Prefeitura</h2>
+            <h2 class="text-2xl font-bold text-gray-800">Painel da Licicon</h2>
             <p class="mt-1 text-sm text-gray-500">Visão geral do sistema</p>
         </div>
 
