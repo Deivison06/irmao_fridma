cfbe241d93bac49aab9c076216626abb356bf445
Deivison Santos <coder08@gsomosocta.com>
2025-09-30 17:44:15 -0300

    Finaliza disponibilidade

--- resources/views/Admin/Processos/pdf/disponibilidade_orçamento.blade.php
+++ resources/views/Admin/Processos/pdf/disponibilidade_orçamento.blade.php
@@ -108,6 +108,116 @@
     {{-- ====================================================================== --}}
     {{-- BLOCO 2: RESUMO DOS DADOS DO PROCESSO --}}
     {{-- ====================================================================== --}}
+    <div>
+        <p style="text-align: center; font-wfont-weight: bold;">DECLARAÇÃO DE COMPATIBILIDADE DA PREVISÃO DE RECURSOS
+            ORÇAMENTÁRIOS</p>
+        <table style="border-collapse: collapse; width: 100%; font-size: 12px;">
+            <thead>
+                <tr>
+                    <th colspan="2"
+                        style="border: 1px solid black; padding: 5px; text-align: center; font-weight: bold;">
+                        RESUMO DOS DADOS DO PROCESSO
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px; font-weight: bold; width: 40%;">N° PROCESSO
+                        ADMINISTRATIVO:</td>
+                    <td style="border: 1px solid black; padding: 5px;">XXX/2025</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px; font-weight: bold;">N° PROCESSO DE CONTRATAÇÃO:
+                    </td>
+                    <td style="border: 1px solid black; padding: 5px;">XXX/2025</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px; font-weight: bold;">OBJETO</td>
+                    <td style="border: 1px solid black; padding: 5px;">XXXXXXXXXX</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px; font-weight: bold;">MODALIDADE:</td>
+                    <td style="border: 1px solid black; padding: 5px;">PREGÃO ELETRÔNICO</td>
+                </tr>
+                <tr>
+                    <td style="border: 1px solid black; padding: 5px; font-weight: bold;">ÓRGÃO RESPONSÁVEL:</td>
+                    <td style="border: 1px solid black; padding: 5px;">SECRETARIA MUNICIPAL DE XXXXXXXX</td>
+                </tr>
+            </tbody>
+        </table>
+
+        <p style="text-indent: 30px;">
+            <strong>DECLARO</strong> para os fins de demonstração da compatibilidade da
+            previsão de recursos orçamentários, com base no
+            art. 72, IV da Lei 14.133/21, que a despesa da respectiva contratação estimada em R$ XXXXXXXXXXXXX
+            (XXXXXXXXXXXXXXXXX)
+            possui previsão de saldo orçamentário e financeiro compatível com a Lei Orçamentária Anual (LOA) e é
+            compatível com o
+            Plano Plurianual (PPA) e com a Lei de Diretrizes Orçamentárias (LDO) vigentes.
+        </p>
+
+        <p style="text-indent: 30px;">
+            As despesas para atender a presente solicitação da demanda, encontram-se amparadas pelo seguinte
+            detalhamento:
+        </p>
+
+        <div style="border: 1px solid black; display: flex; width: 600px; height: 300px;">
+            <div
+                style="flex: 1; padding: 10px; border-right: 1px solid black; font-family: sans-serif; font-size: 14px;">
+                <p style="margin-top: 0; margin-bottom: 10px;">
+                    <strong>Gestão/Unidade:</strong> [...]
+                </p>
+                <p style="margin-bottom: 10px;">
+                    <strong>Fonte de Recursos:</strong> [...]
+                </p>
+                <p style="margin-bottom: 10px;">
+                    <strong>Programa de Trabalho:</strong> [...]
+                </p>
+                <p style="margin-bottom: 10px;">
+                    <strong>Elemento de Despesa:</strong> [...]
+                </p>
+                <p style="margin-bottom: 0;">
+                    <strong>Plano Interno:</strong> [...]
+                </p>
+            </div>
+
+            <div style="flex: 1;">
+            </div>
+        </div>
+
+        <p>OU</p>
+
+        <p style="color: red">
+            Declaro, para os devidos fins, que a presente licitação será realizada sob a forma de <strong>Sistema de
+                Registro de Preços (SRP)</strong>,
+            nos termos do art. 82 e seguintes da Lei nº 14.133/2021.<br>
+            Por se tratar de procedimento que visa apenas ao registro formal de preços, <strong>não há necessidade de
+                indicação de dotação
+                orçamentária nesta fase</strong>, ficando a alocação de recursos vinculada e obrigatória somente no
+            momento da contratação
+            efetiva, mediante emissão da Nota de Empenho correspondente, conforme as demandas das Secretarias/Órgãos
+            requisitantes.<br>
+            Tal medida encontra respaldo legal e visa garantir o adequado planejamento das contratações, respeitando os
+            princípios da
+            eficiência, economicidade e responsabilidade fiscal.
+        </p>
+
+        <p>Encaminhe-se ao DEMANDANTE para a elaboração do TERMO DE REFERÊNCIA</p>
+
+        {{-- Bloco de data e assinatura --}}
+        <div class="footer-signature">
+            {{ \Carbon\Carbon::parse($dataSelecionada)->translatedFormat('d \d\e F \d\e Y') }}
+        </div>
+
+        <div class="signature-block">
+            ___________________________________<br>
+            <strong>
+                {{ $processo->prefeitura->autoridade_competente }} <br>
+                {{ $detalhe->secretaria ?? 'SECRETARIA DE EDUCACAO' }}
+            </strong>
+        </div>
+    </div>
+
 
 </body>
 
